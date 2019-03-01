854ba63d9f85f3ed57a5372ba24826efd9bc5c7c
nisten <nisten@outlook.com>
2019-03-01 15:50:36 -0500

    yarn nvm fix

---
+++ ui/dist/templates/src-pages-blog.634b2115.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{46:function(e,t,n){"use strict";n.r(t);var l=n(1),a=n.n(l),c=n(22),o=n(18);t.default=Object(c.withRouteData)(function(e){var t=e.posts;return a.a.createElement("div",null,a.a.createElement("h1",null,"It's blog time."),a.a.createElement("br",null),"All Posts:",a.a.createElement("ul",null,t.map(function(e){return a.a.createElement("li",{key:e.id},a.a.createElement(o.Link,{to:"/blog/post/".concat(e.id,"/")},e.title))})))})}}]);
+//# sourceMappingURL=src-pages-blog.634b2115.js.map
---
+++ ui/dist/templates/styles.9585c6e4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{109:function(n,w,o){}}]);
+//# sourceMappingURL=styles.9585c6e4.js.map
